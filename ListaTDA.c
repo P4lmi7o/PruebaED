a113ae1b27ef8ff516413c7093a7b4aa6ac29198
P4lmi7o <66698766+P4lmi7o@users.noreply.github.com>
2020-06-15 21:14:28 -0400

    Update ListaTDA.c

--- ListaTDA.c
+++ ListaTDA.c
@@ -1,2 +1,6 @@
 //Primero conseguir el largo de la lista para poder calcular el promedio, luego hay recorrer la lista e ir sumando los datos con un sumador, para finalmente retornar el promedio.
- 
+ float promedio(Lista *lista){
+    int largo, total;
+    largo=Fin(lista);   // largo de la lista
+    total=0;    //sumador
+
